9ba1352db83eba3d54c5e50f38b459d38dc6b7c7
Jason Dellaluce <jasondellaluce@gmail.com>
2023-02-20 14:38:11 +0000

    docs: add falcosecurity copyright notices
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- tests/falco/legacy_test.go
+++ tests/falco/legacy_test.go
@@ -1,3 +1,22 @@
+/*
+Copyright (C) 2023 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+package testfalco
+
 // NOTE: this file is a 1-1 porting of the legacy regression tests
 // implemented in python that we historically have in falcosecurity/falco
 // (see: https://github.com/falcosecurity/falco/tree/059a28184d1d4f498f5b0bd53ffe10d6fedf35c2/test).
@@ -14,8 +33,6 @@
 // - falco_traces.yaml
 // - falco_tests_exceptions.yaml
 //
-
-package testfalco
 
 import (
 	"context"
