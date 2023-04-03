9b9e5bcca71f97f24fb796f4f776273d0ab28d80
Rohith-Raju <rohithraju488@gmail.com>
2023-04-03 16:22:21 +0530

    use embed to read the output file
    Signed-off-by: Rohith-Raju <rohithraju488@gmail.com>

--- tests/data/outputs/event.go
+++ tests/data/outputs/event.go
@@ -1,41 +1,11 @@
 package outputs
 
 import (
-	"encoding/json"
-	"os"
-	"path/filepath"
-	"runtime"
-	"strings"
+	_ "embed"
 
 	"github.com/falcosecurity/testing/pkg/run"
 )
 
-type Data struct {
-	Events []string `json:"events"`
-}
-
-func deserialize() string {
-	_, file, _, ok := runtime.Caller(1)
-	if !ok {
-		panic("not able to extract runtime caller info")
-	}
-	dir, err := filepath.Abs(filepath.Dir(file))
-	if err != nil {
-		panic(err)
-	}
-	filePath := filepath.Join(dir, "events.json")
-	evntfile, err := os.Open(filePath)
-	if err != nil {
-		panic(err)
-	}
-	defer evntfile.Close()
-
-	var events Data
-	err = json.NewDecoder(evntfile).Decode(&events)
-	if err != nil {
-		panic(err)
-	}
-	return strings.Join(events.Events, ",")
-}
-
-var EventData = run.NewStringFileAccessor("eventData", deserialize())
+//go:embed events.txt
+var s string
+var EventData = run.NewStringFileAccessor("eventData", s)
